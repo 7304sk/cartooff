93bcb0f93fbb3794d7f90a330e20f37110a590af
shoalwave <shoalwave.dev@gmail.com>
2022-08-23 16:00:29 +0900

    initial update

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ setuptools.setup(
     name="cartooff",
     version="1.0",
     author="shoalwave",
-    description="Wrapper class of cartopy",
     url="https://github.com/7304sk/cartooff",
+    license='MIT',
+    description="Wrapper class of cartopy",
     packages=setuptools.find_packages(),
+    install_requires=['numpy','pandas','matplotlib','cartopy','mpl_toolkits','shapely','pip'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],
-    license='MIT'
+    ]
 )
