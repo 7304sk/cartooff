fb27302e5bee0b1711f613bc6b4eb1a2f86c2502
shoalwave <shoalwave.dev@gmail.com>
2022-08-23 17:05:00 +0900

    update

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from setuptools import setup, find_packages
-from glob import glob
+import os
+
+def file_walk_relative(top, remove=''):
+    top = top.replace('/', os.path.sep)
+    remove = remove.replace('/', os.path.sep)
+    for root, dirs, files in os.walk(top):
+        for file in files:
+            yield os.path.join(root, file).replace(remove, '')
 
 setup(
     name="cartooff",
@@ -10,7 +17,7 @@ setup(
     description="Wrapper class of cartopy",
     install_requires=['numpy','pandas','matplotlib','shapely','pip','cartopy'],
     packages=find_packages(),
-    package_data={'cartooff': glob('cartooff/data/*')},
+    package_data={'cartooff': list(file_walk_relative('cartooff/data/', remove='cartooff/'))},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
